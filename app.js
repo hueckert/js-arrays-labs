175d3d8278495cf9ded8412e919e327090f5efcf
hueckert <howard.ueckert@protonmail.com>
2024-07-22 23:21:28 -0500

    done

--- app.js
+++ app.js
@@ -233,9 +233,47 @@ const numArrays = [
 	[7, 81, 90]
 ];
 
-const numList = numArray [3][0]
+const numList = numArrays[numArrays.length - 1];
 
 console.log('Exercise 13 result:', numList);
+
+/*
+Exercise 14: Accessing within nested arrays
+
+1) Retrieve the number `66` from the `numArrays` array. As part of this process
+   do not alter the original `numArrays` array.
+
+2) Assign it to a variable called `num`.
+
+Complete Exercise 14 in the space below:
+*/
+
+const num = numArrays [2][1]
+
+console.log('Exercise 14 result:', num);
+
+/*
+Exercise 15: Nested array sum
+
+1) Use nested loops or `forEach()` methods to sum up all numbers within 
+   `numArrays` nested arrays.
+   
+2) Assign the sum to a variable called `total`.
+
+Hint: Be sure to declare and initialize the total variable before the iterations.
+
+Complete Exercise 15 in the space below:
+*/
+
+total = 0;
+
+numArrays.forEach(function(first){
+    first.forEach(function(next) {
+        total += next;
+    })
+});
+
+console.log('Exercise 15 result:\n', total);
 
 
 
